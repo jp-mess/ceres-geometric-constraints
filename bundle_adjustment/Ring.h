92b458d42e34a6942bd727c85e703a439f0906fa
Jack <johnpmesserly@gmail.com>
2023-12-27 12:52:50 -0500

    added ring parameterization problem encoding

--- bundle_adjustment/Ring.h
+++ bundle_adjustment/Ring.h
@@ -6,10 +6,20 @@
 #include <string>
 #include <Eigen/Dense>
 
+#include <iostream> 
+
 #include "ceres/manifold.h"
+#include "ceres/rotation.h"
 
 #include <cmath>
 
+#include <iomanip>
+
+void PrettyPrintArray(const double* array, int size, const std::string& prefix = "Index ") {
+    for (int i = 0; i < size; ++i) {
+        std::cout << prefix << i << ": " << std::fixed << std::setprecision(4) << array[i] << std::endl;
+    }
+}
 
 Eigen::Vector3d ThetaTo3DPoint(double theta, 
                                const Eigen::Vector3d& center, 
@@ -51,48 +61,28 @@ double ProjectPointOntoRing(const Eigen::Vector3d& point,
     return theta;
 }
 
-/*
-class RingManifold : public ceres::Manifold {
-public:
-    RingManifold(const Ring& ring) : ring_(ring) {}
 
-    virtual bool Plus(const double* x, const double* delta, double* x_plus_delta) const override {
-        // Implement the logic to move on the ring
-        return true;
-    }
-
-    //virtual bool ComputeJacobian(const double* x, double* jacobian) const override {
-        // Implement the Jacobian computation
-    //    return true;
-    //}
-
-    virtual int AmbientSize() const override { return 3; } // Size of the space in which the manifold is embedded
-    virtual int TangentSize() const override { return 2; } // Size of a point on the manifold
-
-private:
-    Ring ring_;
-};
-*/
 
 
 class Ring {
-public:
-    Ring(const Eigen::Vector3d& center, const Eigen::Vector3d& normal, double radius, double elevation_degree)
-        : center_(center), normal_(normal), radius_(radius), elevation_degree_(elevation_degree) {}
+    public:
+        Ring() {}
+        Ring(const Eigen::Vector3d& center, const Eigen::Vector3d& normal, double radius, double elevation_degree)
+            : center_(center), normal_(normal), radius_(radius), elevation_degree_(elevation_degree) {}
 
-    // Getters for the ring parameters
-    Eigen::Vector3d center() const { return center_; }
-    Eigen::Vector3d normal() const { return normal_; }
-    double radius() const { return radius_; }
-    double elevation_degree() const { return elevation_degree_; }
+        // Getters for the ring parameters
+        Eigen::Vector3d center() const { return center_; }
+        Eigen::Vector3d normal() const { return normal_; }
+        double radius() const { return radius_; }
+        double elevation_degree() const { return elevation_degree_; }
 
-    // TODO: Implement Plus and Jacobian operations
+        // TODO: Implement Plus and Jacobian operations
 
-private:
-    Eigen::Vector3d center_;
-    Eigen::Vector3d normal_;
-    double radius_;
-    double elevation_degree_;
+    private:
+        Eigen::Vector3d center_;
+        Eigen::Vector3d normal_;
+        double radius_;
+        double elevation_degree_;
 };
 
 
@@ -129,6 +119,126 @@ Ring LoadRingParameters(const std::string& file_path) {
     return Ring(center, normal, radius, elevation_degree);
 }
 
+class RingProblem {
+public:
+    int n_extrinsic = 5;
+    int n_intrinsic = 3;
+    int n_camera_params = n_extrinsic + n_intrinsic;
 
+    RingProblem() {}
+
+    ~RingProblem() {
+        delete[] point_index_;
+        delete[] camera_index_;
+        delete[] observations_;
+        delete[] parameters_;
+    }
+
+    int num_observations() const { return num_observations_; }
+    const double* observations() const { return observations_; }
+    double* mutable_cameras() { return parameters_; }
+    double* mutable_points() { return parameters_ + n_camera_params * num_cameras_; }
+    int num_cameras() const { return num_cameras_; }
+    int num_points() const { return num_points_; }
+    const int* camera_index() const { return camera_index_; }
+    const int* point_index() const { return point_index_; }
+    const double* points() const { return parameters_ + n_camera_params * num_cameras_; }
+    const double* point_for_observation(int i) const { return parameters_ + (n_camera_params * num_cameras_) + (point_index_[i] * 3); }
+    double* mutable_point_for_observation(int i) { return mutable_points() + point_index_[i] * 3; }
+    double* mutable_extrinsic_for_observation(int i) { return mutable_cameras() + camera_index_[i] * n_camera_params; }
+    double* mutable_intrinsic_for_observation(int i) { return mutable_cameras() + camera_index_[i] * n_camera_params + n_extrinsic; }
+    const double* extrinsic_for_observation(int i) const { return parameters_ + camera_index_[i] * n_camera_params; }
+    const double* intrinsic_for_observation(int i) const { return parameters_ + camera_index_[i] * n_camera_params + n_extrinsic; }
+    const Ring& ring() const { return ring_;}
+
+    bool LoadRingFile(const char* ring_file_path) {
+        try {
+            ring_ = LoadRingParameters(std::string(ring_file_path));
+            return true;
+        } catch (const std::runtime_error& e) {
+            std::cout << "Error loading ring file: " << e.what() << std::endl;
+            return false;
+        }
+    }
+
+    bool LoadFile(const char* filename) {
+        FILE* fptr = fopen(filename, "r");
+        if (fptr == nullptr) {
+            return false;
+        }
+
+        FscanfOrDie(fptr, "%d", &num_cameras_);
+        FscanfOrDie(fptr, "%d", &num_points_);
+        FscanfOrDie(fptr, "%d", &num_observations_);
+
+        point_index_ = new int[num_observations_];
+        camera_index_ = new int[num_observations_];
+        observations_ = new double[2 * num_observations_];
+
+        num_parameters_ = n_camera_params * num_cameras_ + 3 * num_points_;
+        parameters_ = new double[num_parameters_];
+
+        for (int i = 0; i < num_observations_; ++i) {
+            FscanfOrDie(fptr, "%d", camera_index_ + i);
+            FscanfOrDie(fptr, "%d", point_index_ + i);
+            for (int j = 0; j < 2; ++j) {
+                FscanfOrDie(fptr, "%lf", observations_ + 2 * i + j);
+            }
+        }
+
+        for (int i = 0; i < num_cameras_; ++i) {
+            double angle_axis[3];
+            for (int j = 0; j < 3; ++j) {
+                FscanfOrDie(fptr, "%lf", &angle_axis[j]);
+            }
+            double quaternion[4];
+            ceres::AngleAxisToQuaternion(angle_axis, quaternion);
+            for (int j = 0; j < 4; ++j) {
+                parameters_[n_camera_params * i + j] = quaternion[j];  // Store quaternion
+            }
+
+            double translation[3];
+            for (int j = 0; j < 3; ++j) {
+                FscanfOrDie(fptr, "%lf", &translation[j]);
+            }
+            Eigen::Vector3d translation_vector(translation[0], translation[1], translation[2]);
+            double theta = ProjectPointOntoRing(translation_vector, ring_.center(), ring_.normal());
+            parameters_[n_camera_params * i + 4] = theta;  // Store theta
+
+            // Store intrinsic parameters
+            for (int j = 0; j < 3; j++) {
+                FscanfOrDie(fptr, "%lf", &parameters_[n_camera_params*i + n_extrinsic + j]);
+            }
+        }
+        // World Points
+        for (int i = 0; i < num_points_; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                FscanfOrDie(fptr, "%lf", &parameters_[n_camera_params * num_cameras_ + 3 * i + j]);
+            }
+        }
+        fclose(fptr);
+        return true;
+    }
+
+private:
+    template <typename T>
+    void FscanfOrDie(FILE* fptr, const char* format, T* value) {
+        int num_scanned = fscanf(fptr, format, value);
+        if (num_scanned != 1) {
+            LOG(FATAL) << "Invalid UW data file.";
+        }
+    }
+
+    int num_cameras_;
+    int num_points_;
+    int num_observations_;
+    int num_parameters_;
+
+    int* point_index_;
+    int* camera_index_;
+    double* observations_;
+    double* parameters_;
+    Ring ring_;
+};
 
 #endif
--- bundle_adjustment/ring_test.cc
+++ bundle_adjustment/ring_test.cc
@@ -1,43 +1,95 @@
-#include "BALProblem.h"
-#include "Ring.h"
+#include "Ring.h" // Assuming this header includes the RingProblem class and necessary dependencies
+#include "ceres/ceres.h"
+#include "ceres/rotation.h"
 #include <iostream>
-#include <Eigen/Dense>
-#include <string>
+
+#include "Ring.h" // Assuming this header includes the RingProblem class and necessary dependencies
+#include <iostream>
+
+void SaveRingProblem(const RingProblem& ring_problem, const std::string& output_filename) {
+    std::ofstream out_file(output_filename);
+    if (!out_file) {
+        std::cerr << "ERROR: unable to open output file " << output_filename << ".\n";
+        return;
+    }
+
+    // Write the header: number of cameras, number of points, number of observations
+    out_file << ring_problem.num_cameras() << " "
+             << ring_problem.num_points() << " "
+             << ring_problem.num_observations() << "\n";
+
+    // Write the observations
+    const double* observations = ring_problem.observations();
+    for (int i = 0; i < ring_problem.num_observations(); ++i) {
+        int camera_index = ring_problem.camera_index()[i];
+        int point_index = ring_problem.point_index()[i];
+        double obs_x = observations[2 * i];
+        double obs_y = observations[2 * i + 1];
+        out_file << camera_index << " " << point_index << " " << obs_x << " " << obs_y << "\n";
+    }
+
+    // Write the optimized camera parameters (extrinsic and intrinsic for each camera)
+    for (int i = 0; i < ring_problem.num_cameras(); ++i) {
+        const double* extrinsic = ring_problem.extrinsic_for_observation(i);
+        double quaternion[4] = {extrinsic[0], extrinsic[1], extrinsic[2], extrinsic[3]};
+        double angle_axis[3];
+        ceres::QuaternionToAngleAxis(quaternion, angle_axis);
+
+        // Write angle-axis representation (3 lines)
+        for (int j = 0; j < 3; ++j) {
+            out_file << angle_axis[j] << "\n";
+        }
+
+        // Convert theta back to translation and write translation (3 lines)
+        double theta = extrinsic[4];  // Theta is the fifth parameter
+        Eigen::Vector3d translation = ThetaTo3DPoint(theta, ring_problem.ring().center(), ring_problem.ring().normal(), ring_problem.ring().radius());
+        out_file << translation.x() << "\n" << translation.y() << "\n" << translation.z() << "\n";
+
+        // Write intrinsic parameters (3 lines)
+        const double* intrinsic = ring_problem.intrinsic_for_observation(i);
+        for (int j = 0; j < 3; ++j) {
+            out_file << intrinsic[j] << "\n";
+        }
+    }
+
+    // Write the points (each coordinate on a separate line)
+    for (int i = 0; i < ring_problem.num_points(); ++i) {
+        const double* point = ring_problem.points() + i * 3;
+        out_file << point[0] << "\n" << point[1] << "\n" << point[2] << "\n";
+    }
+    
+    out_file.close();
+}
 
 int main(int argc, char** argv) {
     if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " BAL_file Ring_params_file" << std::endl;
+        std::cout << "Usage: " << argv[0] << " BAL_file Ring_params_file" << std::endl;
         return 1;
     }
 
-    const std::string bal_file = argv[1];
-    const std::string ring_file = argv[2];
+    const char* bal_file = argv[1];
+    const char* ring_file = argv[2];
 
-    // Load BAL data
-    BALProblem bal_problem;
-    if (!bal_problem.LoadFile(bal_file.c_str())) {
-        std::cerr << "Error loading BAL file." << std::endl;
+    RingProblem ring_problem;
+
+    // Load ring parameters from the file
+    if (!ring_problem.LoadRingFile(ring_file)) {
+        std::cout << "Error loading ring parameters file: " << ring_file << std::endl;
         return 1;
     }
 
-    // Load ring parameters
-    Ring ring = LoadRingParameters(ring_file);
-
-    // Iterate over all cameras
-    for (int i = 0; i < bal_problem.num_cameras(); ++i) {
-        const double* camera = bal_problem.mutable_cameras() + 9 * i; // Assuming 10 parameters per camera
-        Eigen::Vector3d translation(camera[3], camera[4], camera[5]); // Assuming translation parameters
-
-        double theta = ProjectPointOntoRing(translation, ring.center(), ring.normal());
-        Eigen::Vector3d reconstructed_point = ThetaTo3DPoint(theta, ring.center(), ring.normal(), ring.radius());
-
-        double residual = (translation - reconstructed_point).norm();
-
-        std::cout << "Camera " << i << ":" << std::endl;
-        std::cout << "  Original Point: " << translation.transpose() << std::endl;
-        std::cout << "  Reconstructed Point: " << reconstructed_point.transpose() << std::endl;
-        std::cout << "  Residual Distance: " << residual << std::endl;
+    // Load BAL encoding file
+    if (!ring_problem.LoadFile(bal_file)) {
+        std::cout << "Error loading BAL file: " << bal_file << std::endl;
+        return 1;
     }
+
+    // At this point, ring_problem is loaded with the ring parameters and the BAL encoding
+
+    // Further processing can be done here...
+
+    SaveRingProblem(ring_problem, "out.txt");
 
     return 0;
 }
+
